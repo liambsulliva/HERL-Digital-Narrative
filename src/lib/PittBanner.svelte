8f0aca710abf724109bb8363e14cbfa54b419c22
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-09-26 17:19:59 -0400

    properly import banner

--- src/lib/PittBanner.svelte
+++ src/lib/PittBanner.svelte
@@ -1,4 +1,21 @@
-<a href="https://pitt.edu" class="w-32 p-2" aria-label="University of Pittsburgh" >
-  <img src="./src/images/pittbanner-light.png" alt="University of Pittsburgh" class="dark:hidden block">
-  <img src="./src/images/pittbanner-dark.png" alt="University of Pittsburgh" class="dark:block hidden">
+<script lang="ts">
+  import lightbanner from "../images/pittbanner-light.png";
+  import darkbanner from "../images/pittbanner-dark.png";
+</script>
+
+<a
+  href="https://pitt.edu"
+  class="w-32 p-2"
+  aria-label="University of Pittsburgh"
+>
+  <img
+    src={lightbanner}
+    alt="University of Pittsburgh"
+    class="dark:hidden block"
+  />
+  <img
+    src={darkbanner}
+    alt="University of Pittsburgh"
+    class="dark:block hidden"
+  />
 </a>
