bd54f381f8edc0d13a48d88ead68b58ae0add307
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-10-23 18:08:58 -0400

    only enable caption if image loads successfully

--- src/lib/Picture.svelte
+++ src/lib/Picture.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
   export let src: string;
   export let alt: string = "";
+  let imageLoaded: boolean = false;
+
+  function handleLoad() {
+    imageLoaded = true;
+  }
 </script>
 
 <div class="flex flex-col gap-2">
-  <img {src} {alt} class="max-h-[17rem] object-contain m-auto" />
-  {#if alt}
+  <img
+    {src}
+    {alt}
+    on:load={handleLoad}
+    class="max-h-[17rem] object-contain m-auto"
+  />
+  {#if alt && imageLoaded}
     <p class="text-sm text-gray-500">{alt}</p>
   {/if}
 </div>
