7b6052b12f05590b95d2707f7c45f504bdeadfd8
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-11-24 03:43:50 -0500

    introduce line-breaks where relevant

--- src/pages/Page16.svelte
+++ src/pages/Page16.svelte
@@ -13,9 +13,14 @@
         <h3 class="milestone-title">
           CARE OF THE COMBAT AMPUTEE TEXTBOOK PUBLISHED
         </h3>
-        <Description
-          content="The “Care of the Combat Amputee” textbook, revered as the “gold standard” for military healthcare providers, plays a pivotal role in improving the quality of care for Veterans with traumatic amputations. By providing standardized guidelines, it ensures that medical professionals are equipped with the knowledge and skills necessary to deliver the best possible care. In the long-term care and rehabilitation domain, it emphasizes personalized approaches that consider the unique needs of each individual."
-        />
+        <div class="milestone-description flex flex-col gap-2">
+          <Description
+            content="The “Care of the Combat Amputee” textbook, revered as the “gold standard” for military healthcare providers, plays a pivotal role in improving the quality of care for Veterans with traumatic amputations. By providing standardized guidelines, it ensures that medical professionals are equipped with the knowledge and skills necessary to deliver the best possible care."
+          />
+          <Description
+            content="In the long-term care and rehabilitation domain, it emphasizes personalized approaches that consider the unique needs of each individual."
+          />
+        </div>
       </div>
     </div>
   </section>
--- src/pages/Page2.svelte
+++ src/pages/Page2.svelte
@@ -10,11 +10,18 @@
     <h2 class="year-title">1994</h2>
     <div class="milestones-grid">
       <div class="milestone-item">
-        <h3 class="milestone-title">BEGINNING OF HERL'S JOURNEY</h3>
-        <div class="milestone-description">
+        <div class="mb-2">
+          <h3 class="milestone-title !mb-1">BEGINNING OF HERL'S JOURNEY</h3>
+          <i class="text-[#6B7280]">
+            A monumental achievement for veterans and people with disabilities.
+          </i>
+        </div>
+        <div class="milestone-description flex flex-col gap-2">
           <Description
-            content="A monumental achievement for veterans and people with disabilities. The Human Engineering Research Laboratories (HERL) teamed up with the University of Pittsburgh, the Paralyzed Veterans of America (PVA), and the Department of Veterans Affairs (VA) to establish a pioneering lab. This new space was to be located within a VA facility in Highland Drive. The partnership brought together the expertise of researchers, engineers, and medical professionals which leveraged their collective knowledge and resources. This collaboration set the stage for innovative advancements in assistive technology that make a profound impact for individuals with disabilities."
-            length={Infinity}
+            content="The Human Engineering Research Laboratories (HERL) teamed up with the University of Pittsburgh, the Paralyzed Veterans of America (PVA), and the Department of Veterans Affairs (VA) to establish a pioneering lab. This new space was to be located within a VA facility in Highland Drive."
+          />
+          <Description
+            content="The partnership brought together the expertise of researchers, engineers, and medical professionals which leveraged their collective knowledge and resources. This collaboration set the stage for innovative advancements in assistive technology that make a profound impact for individuals with disabilities."
           />
         </div>
       </div>
