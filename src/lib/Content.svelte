48eb764d86a9082e596074f624c506dc85967366
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-12-09 17:19:49 -0500

    refactor Skip Ahead! prompt

--- src/lib/Content.svelte
+++ src/lib/Content.svelte
@@ -137,21 +137,26 @@
 
 <!-- TODO: Refactor layout to take up more space horizontally, we want to reduce scrolling on desktops -->
 <div class="content-container">
-  {#if screenWidth < 800}
-    <div class="rotation-prompt" transition:fade={{ duration: 200 }}>
+  {#if screenWidth < 900}
+    <div class="device-prompt" transition:fade={{ duration: 200 }}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="rotate-icon"
-        viewBox="0 0 24 24"
         width="48"
         height="48"
+        viewBox="0 0 24 24"
       >
         <path
-          fill="currentColor"
-          d="M7.34 6.41L.86 12.9l6.49 6.48 6.49-6.48-6.5-6.49zM3.69 12.9l3.66-3.66L11 12.9l-3.66 3.66-3.65-3.66zm15.67-6.26A8.95 8.95 0 0 0 13 4V.76L8.76 5 13 9.24V6c1.79 0 3.58.68 4.95 2.05a7.007 7.007 0 0 1 0 9.9 6.973 6.973 0 0 1-7.79 1.44l-1.49 1.49C10.02 21.62 11.51 22 13 22c2.3 0 4.61-.88 6.36-2.64a8.98 8.98 0 0 0 0-12.72z"
+          fill="#06060B"
+          d="M20 18c1.1 0 1.99-.9 1.99-2L22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2H0v2h24v-2h-4zM4 6h16v10H4V6z"
         />
       </svg>
-      <p>Please rotate your device to landscape mode</p>
+      <div class="prompt-text">
+        <p class="primary-text">Please switch to a desktop device</p>
+        <p class="secondary-text">
+          For an optimal viewing experience, please use a device with a screen
+          resolution of at least 1440 x 900.
+        </p>
+      </div>
     </div>
   {:else if visible && displayedPage > 0 && displayedPage <= 34}
     <div transition:fade={{ duration: 200 }}>
@@ -182,7 +187,7 @@
     box-sizing: border-box;
   }
 
-  .rotation-prompt {
+  .device-prompt {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -191,18 +196,40 @@
     color: #383844;
     text-align: center;
     gap: 1rem;
+    padding: 2rem;
+    opacity: 1;
   }
 
-  .rotate-icon {
-    animation: rotate 2s infinite;
+  .prompt-text {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
   }
 
-  @keyframes rotate {
+  .primary-text {
+    color: #06060b;
+    font-size: 1.25rem;
+    font-weight: 500;
+  }
+
+  .secondary-text {
+    font-size: 0.875rem;
+    color: #6b7280;
+  }
+
+  svg {
+    animation: pulse 2s infinite;
+  }
+
+  @keyframes pulse {
     0% {
-      transform: rotate(0deg);
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.1);
     }
     100% {
-      transform: rotate(90deg);
+      transform: scale(1);
     }
   }
 </style>
